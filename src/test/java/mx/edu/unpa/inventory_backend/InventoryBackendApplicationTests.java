96399bc9b1127657667060724daec13a7eb861f7
Andres Gonzalez <agonzalez@azurian.com>
2026-04-10 16:56:08 -0600

    chore:proyecto base

---
+++ src/test/java/mx/edu/unpa/inventory_backend/InventoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.unpa.inventory_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
